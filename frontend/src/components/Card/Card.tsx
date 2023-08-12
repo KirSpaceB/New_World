bacd3564b5a6d21d0aec62b148e8dd0bce8ba2c8
KirSpaceB <productivekirk@gmail.com>
2023-08-11 18:27:17 -0700

    feat: finished the plans section

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -16,7 +16,7 @@ export default function Card(props:ICardProps) {
   } = props
   return (
     <div 
-      className=" bg-white p-4 sm:ml-0 h-[60vh] w-[20vw] rounded-xl"
+      className="bg-white p-4 h-[60vh] w-full sm:w-[27vw] 2xl:w-[20vw] rounded-xl"
       style={{
         backgroundColor: cardColor // Make dynamic
       }}
@@ -24,10 +24,10 @@ export default function Card(props:ICardProps) {
       <div className='flex flex-col'>
         <div className={dm_Serif_Display_normal.className}>
           <div className='flex flex-row justify-center items-center'>
-            <span className='text-6xl'>
+            <span className='text-2xl sm:text-4xl 2xl:text-6xl'>
               $
             </span>
-            <h1 className='text-5xl pt-4' id='card_price'>
+            <h1 className='text-3xl sm:text-4xl 2xl:text-5xl pt-4' id='card_price'>
               {price}
             </h1>
           </div>
@@ -35,23 +35,23 @@ export default function Card(props:ICardProps) {
 
         <div className={dm_Serif_Display_italic.className}>
           <div className='flex justify-center items-center'>
-            <h1 className='text-4xl'>
+            <h1 className='text-xl sm:text-2xl 2xl:text-4xl'>
               One time payment
             </h1>
           </div>
-
         </div>
         
         <div className={dm_Mono_normal.className}>
-          <p className='flex justify-start items-start ml-12 mt-6'>
+          <p 
+            className='flex justify-start text-base items-start ml-4 sm:ml-10 2xl:ml-12 mt-6 h-[120px] sm:h-[240px] 2xl:h-[250px] overflow-y-hidden'>
             {cardText}
           </p>
         </div>
 
         <div className='flex items-center justify-center cursor-pointer mt-10'>
           <div className={dm_Mono_normal.className}>
-            <div className='bg-black flex items-center justify-center w-[264px] h-[61px]'>
-              <h3 className='text-2xl text-white'>
+            <div className='bg-black flex items-center justify-center w-full sm:w-[264px] h-[61px]'>
+              <h3 className='text-xl sm:text-2xl text-white'>
                 Buy Now
               </h3>
             </div>
